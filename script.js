faf2e0100f35cc4fb99350ce76bdeb108e5fc1af
mylcevaleksandr <myltsevaleksandr@gmail.com>
2023-01-28 16:01:53 +0300

    JS refactored

--- script.js
+++ script.js
@@ -1,88 +1,10 @@
-// let numberEnter = prompt("enter month name");
-//
-// let daysTotal;
-// console.log(numberEnter);
-//
-// switch (numberEnter) {
-//     case "january":
-//         daysTotal = "31";
-//         break;
-//     case "february":
-//         daysTotal = "29";
-//         break;
-//     case 'march':
-//         daysTotal = "31";
-//         break;
-//     case "april":
-//         daysTotal = "30";
-//         break;
-//     case "may":
-//         daysTotal = "31";
-//         break;
-//     case "june":
-//         daysTotal = "30";
-//         break;
-//     case "july":
-//         daysTotal = "30";
-//         break;
-//     case"august":
-//         daysTotal = "30";
-//         break;
-//     case"september":
-//         daysTotal = "36";
-//         break;
-//     case"october":
-//         daysTotal = "306";
-//         break;
-//     case "november":
-//         daysTotal = "34";
-//         break;
-//     case "december":
-//         daysTotal = "30";
-//         break;
-//     default:
-//         daysTotal = "unknown word";
-//         break;
-// }
-// //
-//
-// let num = 1;
-// let word;
-// switch (num) {
-//     case 1:
-//         word = 'one';
-//         break;
-//     case 2:
-//         word = 'two';
-//         break;
-//     case 3:
-//         word = 'three';
-//         break;
-//     default:
-//         word = 'unknown';
-//         break;
-// }
-// alert(daysTotal + " days in this month.");
 
-
-/* просим пользователя ввести число.*/
-//
-// let numberVar = prompt("Введите любое число.")
-// function integerCheck(number) {
-//
-//     return number === 0 ? "Число равно нулю"
-//         : number > 0 ? "Число положительное"
-//             : number < 0 ? "Число отрицательное"
-//                 : "Недопустимые данные";
-//
-// }
-//
-// /* Проверяем полученные данные. Плюсик нужен, чтоб преобразовать строку в число. Результат проверки сохраняем в переменную testResult и выводим её в alert().*/
-//
-// let testResult = integerCheck(+numberVar);
-//
-// alert(testResult);
-
+let answer = {
+    wife:"",
+    children:"",
+    country:"",
+    year:""
+}
 
 function addFunction() {
     let wife = document.getElementById('womanW').value;
@@ -93,19 +15,6 @@ function addFunction() {
     document.getElementById("joker").innerHTML = "In " + year + " you will be in " + country + " with your beautiful wife " + wife + " and " + children + " children!!!";
 
     document.querySelector(".joker__wrapper").style.display = "flex";
+
+    return {wife,children,country,year}
 }
-
-
-// function Oracle(wife, children, country, year) {
-//
-// }
-//
-// let joke = Oracle(woman, childNumber, targetCountry, futureYear);
-
-//
-// function test() {
-//
-//     alert(`The function 'test' is executed`);
-// }
-//
-// console.log(woman);
